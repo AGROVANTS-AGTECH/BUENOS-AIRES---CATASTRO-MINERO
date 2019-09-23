b539ba9bc0426ed12ee1eb4a0accbec63132c5ab
AGROVANTS <55609306+AGROVANTS-AGTECH@users.noreply.github.com>
2019-09-23 04:20:45 -0300

    Add files via upload

---
+++ data/NuevosBAP4_2.js
@@ -0,0 +1,8 @@
+var json_NuevosBAP4_2 = {
+"type": "FeatureCollection",
+"name": "NuevosBAP4_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "nombre": "CANTERA MINUJIN", "mineral": "MICA", "productor": "LUIS A y AGUSTIN A MINUJIN - 448", "estado": "ACTIVA", "nom__catas": "TORNQUINST. Circ III. Parc 39t", "superficie": "196.57443027087382", "tipo derec": null, "titular": null, "pertn.": null, "reservas": null, "name": null, "codif.": null, "expediente": null, "tipo der_1": null, "t_categor�": null, "sustancia": null, "titular_2": null, "estado_2": null, "partido": null, "pertn._2": null, "reservas_2": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -62.3078698087416, -38.333490900886687 ], [ -62.292107723839521, -38.320902518682693 ], [ -62.283816691308807, -38.327390265590651 ], [ -62.299904342958008, -38.339720554117996 ], [ -62.3078698087416, -38.333490900886687 ] ] ] ] } }
+]
+}
